f6cb8e1246257f492ae77eeca8e939b1cae572d1
barney nicholls@ <barney.nicholls@gmail.com>
2024-04-06 10:35:12 +0100

    add dht 11

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ unsigned long myChannelNumber = SECRET_CH_ID;
 const char *myWriteAPIKey = SECRET_WRITE_APIKEY;
 
 // sensors
+
+// soil
 #define AOUT_PIN A6 // Arduino pin that connects to AOUT pin of moisture sensor (D34 maps to ADC6)
 // this ADC pin must not be on ADC2 circuit.  Try a pin above for example D33+
 
@@ -25,6 +27,20 @@ float SoilMoisturePercentage = 0;
 int high = 0;
 int low = 9999;
 
+// air
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 23 // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT11 // DHT 11
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+float temp;
+float humidity;
+
 // loop timings
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5 * 60000;
@@ -34,6 +50,8 @@ unsigned long totalMoisture = 0;
 void setup()
 {
   Serial.begin(9600);
+
+  dht.begin();
 
   WiFi.begin(ssid, password);
 
@@ -53,6 +71,28 @@ void loop()
   high = max(MoistureLevel, high);
   low = min(MoistureLevel, low);
 
+  // air
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature))
+  {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else
+  {
+    temp = event.temperature;
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity))
+  {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else
+  {
+    humidity = event.relative_humidity;
+  }
+
   // debug region start
   SoilMoisturePercentage = map(MoistureLevel, OpenAirReading, WaterReading, 0, 100);
   SoilMoisturePercentage = constrain(SoilMoisturePercentage, 0, 100);
@@ -64,7 +104,14 @@ void loop()
   Serial.print(" low: ");
   Serial.print(low);
   Serial.print(" high: ");
-  Serial.println(high);
+  Serial.print(high);
+  Serial.print(" Temp: ");
+  Serial.print(temp);
+  Serial.print("C Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+  
+
   // debug region end
 
   if ((millis() - lastTime) > timerDelay)
@@ -80,6 +127,8 @@ void loop()
     ThingSpeak.setField(1, SoilMoisturePercentage);
     ThingSpeak.setField(2, low);
     ThingSpeak.setField(3, high);
+    ThingSpeak.setField(4, temp);
+    ThingSpeak.setField(5, humidity);
     int result = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
 
     Serial.print("Thingspeak: ");
